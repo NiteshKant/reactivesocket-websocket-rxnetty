d678560007238f7f9f962b766323325257742fc4
Ben Christensen <benjchristensen@gmail.com>
2015-07-26 17:20:40 -0700

    Client Bindings
    The client/server pair now works together using the reactivesocket-java protocol implementations.
    See the ClientServerTest unit test for examples.

---
+++ src/main/java/io/reactivesocket/websocket/rxnetty/ReactiveSocketWebSocketClient.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright 2015 Netflix, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.reactivesocket.websocket.rxnetty;
+
+import static rx.RxReactiveStreams.toObservable;
+import static rx.RxReactiveStreams.toPublisher;
+
+import org.reactivestreams.Publisher;
+
+import io.netty.buffer.Unpooled;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.reactivesocket.DuplexConnection;
+import io.reactivesocket.Message;
+import io.reactivesocket.ReactiveSocketClientProtocol;
+import io.reactivex.netty.protocol.http.client.HttpClient;
+import io.reactivex.netty.protocol.http.ws.WebSocketConnection;
+import io.reactivex.netty.protocol.http.ws.client.WebSocketResponse;
+import rx.Observable;
+import rx.Single;
+
+public class ReactiveSocketWebSocketClient {
+
+    private final ReactiveSocketClientProtocol rsProtocol;
+
+    private ReactiveSocketWebSocketClient(WebSocketConnection wsConn) {
+        this.rsProtocol = ReactiveSocketClientProtocol
+                .create(new DuplexConnection() {
+
+                    @Override
+                    public Publisher<Message> getInput() {
+                        return toPublisher(wsConn.getInput().map(frame -> {
+                            return Message.from(frame.content().nioBuffer());
+                        }));
+                    }
+
+                    @Override
+                    public Publisher<Void> write(Publisher<Message> o) {
+                        // had to use writeAndFlushOnEach instead of write for frames to get through
+                        // TODO determine if that's expected or not
+                        return toPublisher(wsConn.writeAndFlushOnEach(toObservable(o).map(m -> {
+                            // return new BinaryWebSocketFrame(Unpooled.wrappedBuffer(m.getBytes()));
+                            return new TextWebSocketFrame(Unpooled.wrappedBuffer(m.getBytes()));
+                        })));
+                    }
+
+                });
+    }
+
+    public static ReactiveSocketWebSocketClient create(WebSocketConnection ws) {
+        return new ReactiveSocketWebSocketClient(ws);
+    }
+
+    public Single<String> requestResponse(String request) {
+        return toObservable(rsProtocol.requestResponse(request)).toSingle();
+    }
+
+    public Observable<String> requestStream(String request) {
+        return toObservable(rsProtocol.requestStream(request));
+    }
+
+    public Observable<String> requestSubscription(String topic) {
+        return toObservable(rsProtocol.requestSubscription(topic));
+    }
+
+    public Single<Void> fireAndForget(String request) {
+        return toObservable(rsProtocol.fireAndForget(request)).toSingle();
+    }
+
+}
---
+++ src/test/java/io/reactivesocket/websocket/rxnetty/ClientServerTest.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright 2015 Netflix, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.reactivesocket.websocket.rxnetty;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.netty.buffer.ByteBuf;
+import io.reactivex.netty.protocol.http.client.HttpClient;
+import io.reactivex.netty.protocol.http.server.HttpServer;
+import io.reactivex.netty.protocol.http.ws.client.WebSocketResponse;
+import rx.Observable;
+import rx.Single;
+import rx.observers.TestSubscriber;
+
+public class ClientServerTest {
+
+    static Observable<ReactiveSocketWebSocketClient> client;
+    static HttpServer<ByteBuf, ByteBuf> server;
+
+    @BeforeClass
+    public static void setup() {
+        ReactiveSocketWebSocketServer serverHandler = ReactiveSocketWebSocketServer.create(
+                requestResponse -> {
+                    return Single.just(requestResponse + " world");
+                } ,
+                requestStream -> {
+                    return Observable.just(requestStream, "world");
+                } , null, null);
+
+        server = HttpServer.newServer().start((req, resp) -> {
+            return resp.acceptWebSocketUpgrade(serverHandler::acceptWebsocket);
+        });
+
+        client = HttpClient.newClient("localhost", server.getServerPort())
+                .createGet("/rs")
+                .requestWebSocketUpgrade()
+                .flatMap(WebSocketResponse::getWebSocketConnection)
+                .map(ReactiveSocketWebSocketClient::create);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        server.shutdown();
+    }
+
+    @Test
+    public void testRequestResponse() {
+        TestSubscriber<String> ts = TestSubscriber.create();
+        client.flatMap(reactiveSocket -> {
+            return reactiveSocket.requestResponse("hello").toObservable();
+        }).subscribe(ts);
+        ts.awaitTerminalEvent(500, TimeUnit.MILLISECONDS);
+        ts.assertValue("hello world");
+    }
+
+    @Test
+    public void testRequestResponseMultiple() {
+        TestSubscriber<String> ts = TestSubscriber.create();
+        client.flatMap(reactiveSocket -> {
+            return reactiveSocket.requestResponse("hello")
+                    .mergeWith(reactiveSocket.requestResponse("hi"));
+        }).subscribe(ts);
+        ts.awaitTerminalEvent(500, TimeUnit.MILLISECONDS);
+        ts.assertValues("hello world", "hi world");
+    }
+
+    @Test
+    public void testRequestStream() {
+        TestSubscriber<String> ts = TestSubscriber.create();
+        client.flatMap(reactiveSocket -> {
+            return reactiveSocket.requestStream("hello");
+        }).subscribe(ts);
+        ts.awaitTerminalEvent(500, TimeUnit.MILLISECONDS);
+        ts.assertValues("hello", "world");
+    }
+
+}
